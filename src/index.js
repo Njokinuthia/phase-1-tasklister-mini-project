bc99764359f8bfa2849dc2a3754381f603bcf4b0
Njoki <kinuthiash@gmail.com>
2022-04-23 20:55:43 +0300

    add delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector("form#create-task-form")
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    let input = document.querySelector("input#new-task-description")
+    logToDoTask(input.value)
+    form.reset()
+  })
 });
+
+function logToDoTask(toDoTask) {
+  let toDoList = document.querySelector("ul#tasks")
+  let list = document.createElement("li")
+  let button = document.createElement("button")
+  button.addEventListener("click",deleteList)
+  button.innerHTML="x"
+  list.textContent = `${toDoTask} `
+  toDoList.appendChild(list)  
+  list.appendChild(button)
+  console.log(toDoList)
+}
+
+function deleteList(e){
+  e.target.parentNode.remove()
+}
